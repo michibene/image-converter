598fedc369bfa862c5dc49e00d65efb9c37b85c6
Michal <michal.benoo@gmail.com>
2022-12-16 17:07:28 +0100

    Add absolute paths mapping for imports To write simpler imports and be able to refactor easily in future

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
+import NavigationHeader from "navigation/NavigationHeader";
+
 function App() {
     return (
         <div className="App">
+            <NavigationHeader />
+
             <h1>Convert a color image to grayscale online</h1>
         </div>
     );
---
+++ frontend/src/components/ui/navigation/NavigationHeader.tsx
@@ -0,0 +1,11 @@
+import photoneoLogo from "assets/Photoneo_logo_horizontal_color.svg";
+
+function NavigationHeader() {
+    return (
+        <header>
+            <img src={photoneoLogo} alt="React Logo" />
+        </header>
+    );
+}
+
+export default NavigationHeader;
