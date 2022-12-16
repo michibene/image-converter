336f581532002b136b6d59ecaee2f527341865b4
Michal <michal.benoo@gmail.com>
2022-12-16 16:35:55 +0100

    Add Tailwind CSS for styling

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                fontLightColor: "#7989a6",
+                fontDarkestColor: "#1a1919",
+                primaryColor: "#0091d0",
+                primaryDarkerColor: "#0577a9",
+            },
+        },
+    },
+    plugins: [],
+};
